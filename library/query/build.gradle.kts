5447c5994fcb3568ec13113616f78a2af4997a00
Tamim Attafi <tamim.attafi@i-link.pro>
2024-03-13 16:38:43 +0700

    config: add maven publishing script

--- library/query/build.gradle.kts
+++ library/query/build.gradle.kts
@@ -1,6 +1,8 @@
+import com.vanniktech.maven.publish.SonatypeHost
+
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
-    id(libs.plugins.convention.publication.get().pluginId)
+    alias(libs.plugins.maven.publish)
 }
 
 kotlin {
@@ -31,4 +33,42 @@ kotlin {
         sourceCompatibility = JavaVersion.VERSION_17
         targetCompatibility = JavaVersion.VERSION_17
     }
+}
+
+mavenPublishing {
+    val version = "0.1.0-local82"
+    val groupId = "com.attafitamim.kabin"
+    val artifact = "query"
+
+    coordinates(groupId, artifact, version)
+
+    pom {
+        name.set("Kabin")
+        description.set("Database library for Kotlin Multiplatform (Android/iOS/JVM/JS)")
+        url.set("https://github.com/tamimattafi/kabin")
+
+        licenses {
+            license {
+                name.set("Library Licence")
+                url.set("https://github.com/tamimattafi/kabin/blob/main/LICENSE")
+            }
+        }
+
+        developers {
+            developer {
+                id.set("attafitamim")
+                name.set("Tamim Attafi")
+                email.set("attafitamim@gmail.com")
+            }
+        }
+
+        scm {
+            connection.set("scm:git:github.com/tamimattafi/kabin.git")
+            developerConnection.set("scm:git:ssh://github.com/tamimattafi/kabin.git")
+            url.set("https://github.com/tamimattafi/kabin/tree/main")
+        }
+    }
+
+    publishToMavenCentral(SonatypeHost.CENTRAL_PORTAL)
+    signAllPublications()
 }
