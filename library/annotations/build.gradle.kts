238bda5b09b3c8aa95e5f760f50525cde52430c3
Tamim Attafi <tamim.attafi@i-link.pro>
2024-03-24 00:43:27 +0700

    config: move publishing script to convention plugin

--- library/annotations/build.gradle.kts
+++ library/annotations/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id(libs.plugins.convention.multiplatform.get().pluginId)
+    id(libs.plugins.convention.publish.get().pluginId)
 }
 
 kotlin {
--- library/query/build.gradle.kts
+++ library/query/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     alias(libs.plugins.kotlin.multiplatform)
+    id(libs.plugins.convention.publish.get().pluginId)
 }
 
 kotlin {
