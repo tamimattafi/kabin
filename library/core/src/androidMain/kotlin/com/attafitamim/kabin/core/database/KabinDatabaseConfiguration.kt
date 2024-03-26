db530a3485071c3a0cdbf994c8703da2775c0748
Tamim Attafi <tamim.attafi@i-link.pro>
2024-03-26 17:44:15 +0700

    feat: add support for foreign key configuration
    Fixes #5

--- library/core/src/androidMain/kotlin/com/attafitamim/kabin/core/database/KabinDatabaseConfiguration.kt
+++ library/core/src/androidMain/kotlin/com/attafitamim/kabin/core/database/KabinDatabaseConfiguration.kt
@@ -1,8 +1,17 @@
 package com.attafitamim.kabin.core.database
 
 import android.content.Context
+import androidx.sqlite.db.SupportSQLiteDatabase
 
 private const val DEFAULT_CACHE_SIZE = 20
+
+typealias OpenCallback = (db: SupportSQLiteDatabase) -> Unit
+
+private fun createDefaultCallback(
+    foreignKeyConstraintsEnabled: Boolean
+): OpenCallback = { db ->
+    db.setForeignKeyConstraintsEnabled(foreignKeyConstraintsEnabled)
+}
 
 actual class KabinDatabaseConfiguration(
     val context: Context,
@@ -10,4 +19,6 @@ actual class KabinDatabaseConfiguration(
     val cacheSize: Int = DEFAULT_CACHE_SIZE,
     val useNoBackupDirectory: Boolean = false,
     val windowSizeBytes: Long? = null,
+    val foreignKeyConstraintsEnabled: Boolean = true,
+    val onOpen: OpenCallback? = createDefaultCallback(foreignKeyConstraintsEnabled)
 )
